b8a77f596f04a8bac57209a7446dc0288916baa7
Survive <survive@LT13214969.local>
2020-09-28 15:34:07 +0530

    Count of subset sum with given sum

--- dataStructureAlgorithms/src/main/java/problems/dp/CountOfSubsetSum.java
+++ dataStructureAlgorithms/src/main/java/problems/dp/CountOfSubsetSum.java
@@ -12,23 +12,46 @@ public class CountOfSubsetSum {
             return count + 1;
         }
 
-        if(mem[n][sum]>0){
+        if (mem[n][sum] > 0) {
             return mem[n][sum];
         }
         if (arr[n] <= sum) {
-            mem[n][sum] =  count(arr, sum - arr[n], n - 1, count, mem) + count(arr, sum, n - 1, count, mem);
+            mem[n][sum] = count(arr, sum - arr[n], n - 1, count, mem) + count(arr, sum, n - 1, count, mem);
         } else {
-            mem[n][sum] =  count(arr, sum, n - 1, count, mem);
+            mem[n][sum] = count(arr, sum, n - 1, count, mem);
         }
 
         return mem[n][sum];
     }
 
-    public static void main(String[] args) {
-        int arr[] = {3, 3, 3, 3};
-        int x=6;
+    public static int topDownApproach(int arr[], int sum) {
+        if (sum == 0) {
+            return 1;
+        }
 
-        int mem[][] = new int[arr.length+1][x+1];
-        System.out.println(count(arr, x, arr.length-1, 0, mem));
+        int n = arr.length;
+        int holder[][] = new int[n + 1][sum + 1];
+        holder[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (j == 0) {
+                    holder[i][j] = 1;
+                } else if (j < arr[i - 1]) {
+                    holder[i][j] = holder[i - 1][j];
+                } else {
+                    holder[i][j] = holder[i - 1][j] + holder[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+        return holder[n][sum];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 3};
+        int x = 6;
+
+        int mem[][] = new int[arr.length + 1][x + 1];
+        System.out.println("Recursive Approach: " + count(arr, x, arr.length - 1, 0, mem));
+        System.out.println("Top Down Approach: " + topDownApproach(arr, x));
     }
 }
--- dataStructureAlgorithms/src/main/java/problems/dp/PartitionProblem.java
+++ dataStructureAlgorithms/src/main/java/problems/dp/PartitionProblem.java
@@ -7,16 +7,39 @@ import utils.ArrayUtils;
  */
 public class PartitionProblem {
 
-    public static boolean find(int arr[], int n){
+    public static boolean find(int arr[], int n) {
 
         int sum = ArrayUtils.sum(arr);
-        if(sum%2!=0){
+        if (sum % 2 != 0) {
             return false;
         }
 
-        int findSum = sum/2;
-        boolean mem[][] = new boolean[n+1][findSum+1];
-        return SubsetSumProblem.find(arr, n-1, sum/2, mem);
+        int findSum = sum / 2;
+        boolean mem[][] = new boolean[n + 1][findSum + 1];
+        return subsetSum(arr, findSum);
+    }
+
+    public static boolean subsetSum(int arr[], int sum) {
+        if (sum == 0) {
+            return true;
+        }
+
+        int n = arr.length;
+        boolean holder[][] = new boolean[n + 1][sum + 1];
+        holder[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (j == 0) {
+                    holder[i][j] = true;
+                } else if (j < arr[i - 1]) {
+                    holder[i][j] = holder[i - 1][j];
+                } else {
+                    holder[i][j] = holder[i - 1][j] || holder[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+
+        return holder[n][sum];
     }
 
     public static void main(String[] args) {
